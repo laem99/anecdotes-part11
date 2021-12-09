70fd49eaeb8f3a7729c089dbe044b347e0256f71
laem99 <c9laem00@students.oamk.fi>
2021-12-09 12:30:06 +0200

    11.21.12

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     'browser': true,
     'node': true,
     'es6': true,
+    'jest/globals': true,
     'cypress/globals': true,
   },
   'extends': [
@@ -17,7 +18,7 @@ module.exports = {
     'sourceType': 'module'
   },
   'plugins': [
-    'react', 'cypress'
+    "react", "jest", "cypress"
   ],
   'rules': {
     'indent': [
