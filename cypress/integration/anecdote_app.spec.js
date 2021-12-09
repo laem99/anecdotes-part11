8254b5b20ff134f8872fe082618c9b5b0863ee19
laem99 <c9laem00@students.oamk.fi>
2021-12-09 14:12:22 +0200

    11.21.33

--- cypress/integration/anecdote_app.spec.js
+++ cypress/integration/anecdote_app.spec.js
@@ -1,6 +1,6 @@
 describe('Anecdote app', function () {
     it('front page can be opened', function () {
-        cy.visit('http://localhost:3000')
+        cy.visit('http://localhost:3001')
         cy.contains('Anecdotes')
         cy.contains('If it hurts, do it more often')
     })
