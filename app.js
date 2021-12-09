2805d5118e4dbe28bfbde2017c82abdf076b0978
laem99 <c9laem00@students.oamk.fi>
2021-12-09 14:53:29 +0200

    11.21.39

--- app.js
+++ app.js
@@ -10,5 +10,9 @@ const wares = server.defaults({
 app.use(wares)
 app.use(router)
 
+app.get('/heath', (req, res) => {
+  res.send('ok')
+})
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
