de5e3ba94f02e43df64f96a4a037dd38c92cbb42
laem99 <c9laem00@students.oamk.fi>
2021-12-09 14:01:57 +0200

    11.21.31

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const server = require('json-server')
 const app = server.create()
 const router = server.router('db.json')
+const path = require('path')
 
+const wares = server.defaults({
+  static: path.join(__dirname, './build')
+})
+
+app.use(wares)
 app.use(router)
 
 const PORT = process.env.PORT || 3001
